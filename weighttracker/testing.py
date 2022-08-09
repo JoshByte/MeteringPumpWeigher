b3e40a1b694bd163fb4240bc947adc6eae1f8a89
Joshua Snyder <74687662+JoshByte@users.noreply.github.com>
2022-08-09 14:30:01 -0500

    Changed testing to look for not equal to rather then looking for equal to. That seems to have made it easier. It is getting closer just need to make more adjustments of the main while loop and then ill be able to work on cleanup.

--- weighttracker/testing.py
+++ weighttracker/testing.py
@@ -3,6 +3,9 @@
 
 # I might be able to get rid a bunch of the code in main by just running a loop on the first_weight asking for weight until the list has 3 readings. That will clean up my main function.
 
+from tabnanny import check
+
+
 TOLERANCE_WITHIN_WEIGHTS = 0.02 # percent
 
 class Pump:
@@ -56,27 +59,37 @@ def main():
         weight_in_grams = float(input("Enter the weight reading in grams: "))
         if len(meteringpump_weights) != 3:
 
+            print(f"The weight {weight_in_grams} has been saved.")
+            meteringpump_weights.append(weight_in_grams)
+
+            # I wonder if theres a cleaner way to have the next 3 lines or is this okay.
+            acceptable_weight_range = TOLERANCE_WITHIN_WEIGHTS * meteringpump_weights[0] # Changed to list index 0 no longer the first weight. If the first weight is replaced then this would be a problem
+            highest_acceptable_range = acceptable_weight_range + meteringpump_weights[0] # Changed to list index 0 no longer the first weight. If the first weight is replaced then this would be a problem
+            lowest_acceptable_range = meteringpump_weights[0] - acceptable_weight_range # Changed to list index 0 no longer the first weight. If the first weight is replaced then this would be a problem
+
+            print(f"The next readings have to be greater than {weight_in_grams - acceptable_weight_range} and less than {weight_in_grams + acceptable_weight_range}\n")
+
+            check_range(weight_in_grams, highest_acceptable_range, lowest_acceptable_range)
+
+            continue
+
+        else:
             print("All three weights have been saved. Here they are:\n")
             average_weight = (meteringpump_weights[0] + meteringpump_weights[1] + meteringpump_weights[2]) / len(meteringpump_weights) # Did the length of the list rather than have a magic number.
             metering_pump_undertest = Pump(meteringpump_weights[0], meteringpump_weights[1], meteringpump_weights[2], TOLERANCE_WITHIN_WEIGHTS, average_weight)
             metering_pump_undertest.tellData()
             break
-        elif weight_in_grams <= highest_acceptable_range and weight_in_grams >= lowest_acceptable_range:
-            print("The weight has past the 2% allowed range. This is now the first reading.")
-            meteringpump_weights.clear() # This will empty the list of all items
-            meteringpump_weights.append(weight_in_grams) # After having a clean list I will add this new weight as the "first one" or the one were gonna have the accepted range determined by.
-            continue
-        else:
-            print("The weight has been saved. Can move on to the next weight.")
-            meteringpump_weights.append(weight_in_grams)
 
-            # I wonder if theres a cleaner way to have the next 3 lines. Also the placement feels weird
-            acceptable_weight_range = TOLERANCE_WITHIN_WEIGHTS * meteringpump_weights[0] # Changed to list index 0 no longer the first weight. If the first weight is replaced then this would be a problem
-            highest_acceptable_range = acceptable_weight_range + meteringpump_weights[0] # Changed to list index 0 no longer the first weight. If the first weight is replaced then this would be a problem
-            lowest_acceptable_range = meteringpump_weights[0] - acceptable_weight_range # Changed to list index 0 no longer the first weight. If the first weight is replaced then this would be a problem
+def check_range(weight, highest_range, lowest_range):
+    range_calculation = weight <= highest_range and weight >= lowest_range
 
-            print(f"The next readings have to be greater {weight_in_grams - acceptable_weight_range} and less than {weight_in_grams + acceptable_weight_range}\n")
-            continue
+    if range_calculation == True:
+        print("The weight is within the 2% range. Ready for the next weight.")
+    else:
+        print("The weight has past the 2% allowed range. This is now the first reading.")
+        meteringpump_weights.clear() # This will empty the list of all items
+        meteringpump_weights.append(weight) # After having a clean list I will add this new weight as the "first one" or the one were gonna have the accepted range determined by.
+
 
 if __name__ == '__main__':
     main()
